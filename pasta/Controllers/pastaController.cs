a0ccecc3eef2f43db6d7e7382902f68cd765ec30
Clarasamy <44640744+Clarasamy@users.noreply.github.com>
2018-11-14 20:35:59 +0200

    Initial commit

---
+++ pasta/Controllers/pastaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pasta.Controllers
+{
+    public class pastaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
